bd39b17e014c14de2393f73cdcd8cbb31f1a7368
bariskin <chiurcciu@mail.ru>
2023-03-01 21:06:03 +0300

    added panel and frame

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,7 +1,30 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class JavaSweeper extends JFrame {
+    private JPanel panel;
     public static void main(String[] args) {
-        new JavaSweeper().setVisible(true);
+        new JavaSweeper();
+    }
+    private JavaSweeper()
+    {
+        initPanel();
+        initFrame();
+    }
+    private void initFrame()
+    {
+       pack();
+       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+       setTitle("Java Sweeper");
+       setLocationRelativeTo(null);
+       setResizable(false);
+       setVisible(true);
+    }
+
+    private void initPanel()
+    {
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500,500));
+        add(panel);
     }
 }
