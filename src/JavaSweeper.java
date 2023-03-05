89ff416dc9cc75748a21c2d7b040f87517630ec3
bariskin <chiurcciu@mail.ru>
2023-03-05 15:07:53 +0300

    1. added box image 2. create one line images

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,25 +1,37 @@
 import javax.swing.*;
 import java.awt.*;
+import sweeper.Box;
 
 public class JavaSweeper extends JFrame {
     private JPanel panel;
-    public static
-    void main(String[] args) {
+    private final int COLS = 15;
+    private final int ROWS = 1;
+    private final int IMAGE_SIZE = 50;
+
+    public static void main(String[] args) {
         new JavaSweeper();
     }
     private JavaSweeper() // конструктор
     {
+        setImages();
         initPanel();
         initFrame();
     }
 
     private void initPanel()
     {
-        panel = new JPanel();
-        panel.setPreferredSize(new Dimension(500,500));
+        panel = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g)
+            {
+                super.paintComponent(g);
+                for (Box box: Box.values())
+                    g.drawImage((Image) box.image, box.ordinal() * IMAGE_SIZE, 0, this);
+            }
+        };
+        panel.setPreferredSize(new Dimension(COLS * IMAGE_SIZE,ROWS * IMAGE_SIZE));
         add(panel);
     }
-
     private void initFrame()
     {
        pack();
@@ -28,5 +40,16 @@ public class JavaSweeper extends JFrame {
        setLocationRelativeTo(null);
        setResizable(false);
        setVisible(true);
+    }
+    private void setImages()
+    {
+        for (Box box: Box.values())
+            box.image = getImage(box.name().toLowerCase());
+    }
+    private Image getImage(String name)
+    {
+        String filename = "img/" + name + ".png";
+        ImageIcon icon = new ImageIcon(getClass().getResource(filename));
+        return icon.getImage();
     }
 }
